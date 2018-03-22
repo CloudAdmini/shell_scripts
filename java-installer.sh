bd35da9f38d82c16276f3cf4a9d8f5ec81ce0400
CloudAdmini <36741073+CloudAdmini@users.noreply.github.com>
2018-03-22 16:16:52 +0000

    Update java-installer.sh

--- java-installer.sh
+++ java-installer.sh
@@ -64,7 +64,7 @@ if [[ "$JAVA_CHECK" == *"Java(TM) SE Runtime Environment"* ]]; then
    echo
    echo "Java is successfully installed!"
    echo
-   java -version 2>> java-output
+   echo $JAVA_CHECK
    echo
    exit 0
 else
